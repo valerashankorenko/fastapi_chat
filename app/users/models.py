78e264d547f95335b03f23fdbb0dcf75587d5856
Valeron <valerashankorenko@yandex.by>
2024-11-24 17:57:33 +0300

    Создание моделей

---
+++ app/users/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
